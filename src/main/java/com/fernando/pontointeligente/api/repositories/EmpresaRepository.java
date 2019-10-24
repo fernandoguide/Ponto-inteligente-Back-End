01c8f1f875bce8e67e34c207e7c7042daa9a7920
fernandoguide <fernando-guide@hotmail.com>
2019-10-24 10:22:55 -0300

    refactor all

---
+++ src/main/java/com/fernando/pontointeligente/api/repositories/EmpresaRepository.java
@@ -0,0 +1,13 @@
+package com.fernando.pontointeligente.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fernando.pontointeligente.api.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
